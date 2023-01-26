f13c6de6cb918779c207adc2b469606a3411872e
Kyloren21 <xyantsbusiness@gmail.com>
2023-01-26 11:13:49 +1100

    update

---
+++ js/js.sh
@@ -0,0 +1,3 @@
+echo "export SENDGRID_API_KEY='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'" > sendgrid.env
+echo "sendgrid.env" >> .gitignore
+source ./sendgrid.env
--- js/scripts.js
+++ js/scripts.js
@@ -4,4 +4,25 @@
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-business-frontpage/blob/master/LICENSE)
 */
 // This file is intentionally blank
-//x Use this file to add JavaScript to your project
+// Use this file to add JavaScript to your project
+
+// using Twilio SendGrid's v3 Node.js Library
+// https://github.com/sendgrid/sendgrid-nodejs
+javascript
+const sgMail = require('@sendgrid/mail')
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+const msg = {
+  to: 'transrapidl0business@gmail.com', // Change to your recipient
+  from: 'kenuginige@gmail.com', // Change to your verified sender
+  subject: 'Sending with SendGrid is Fun',
+  text: 'and easy to do anywhere, even with Node.js',
+  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+}
+sgMail
+  .send(msg)
+  .then(() => {
+    console.log('Email sent')
+  })
+  .catch((error) => {
+    console.error(error)
+  })
